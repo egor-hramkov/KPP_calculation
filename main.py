e718deb8d2ea2d6354e4c28d2a9aa77704b2f98b
a.prediger <a.prediger@ritg.ru>
2023-11-01 19:57:24 +0400

    Структурирован файл main, закончена разработка над SpeedCarService, добавлены параметры замеров ноютон метра и лошадиных сил у двигателя автомобиля.

--- main.py
+++ main.py
@@ -1,37 +1,45 @@
 import json
-
 import numpy as np
 import pandas as pd
-
+from speed_car_service import SpeedCarService
 from turnovers_wheel_service import TurnoversWheelsService
 
+#инициализация массива со значениями тех оборотов, с которыми придётся работать
 frequency_turns_per_min = np.linspace(600, 4200, 19)
 
+#загружаем данные с файла конфига
 file_json = open('source/config.json')
 config = json.load(file_json)
 
+#формирование данных, где вычислются обороты колёс в минуту относительно кол-ва оборотов двигателя, номера передачи,
+#данных о передаточных числах каждой скорости (таблица из ecxel №2)
 gear_rations = config['data']['gear_ratio']
 
-a = TurnoversWheelsService(
-    hub1=gear_rations['hub_1'],
-    hub2=gear_rations['hub_2'],
-    hub3=gear_rations['hub_3'],
-    hub4=gear_rations['hub_4'],
-    hub5=gear_rations['hub_5'],
-    hub_reverse=gear_rations['hub_reverse'],
-    transfer_case=gear_rations['transfer_case'],
-    on_board_gearbox=gear_rations['on_board_gearbox'],
-    main_pair=gear_rations['main_pair'],
-    frequency_turns_per_min=list(frequency_turns_per_min)
-)
+turns_wheels_service = TurnoversWheelsService(gear_rations['hub_1'], gear_rations['hub_2'], gear_rations['hub_3'], gear_rations['hub_4'],
+                           gear_rations['hub_5'], gear_rations['hub_reverse'], gear_rations['transfer_case'],
+                           gear_rations['on_board_gearbox'],
+                           gear_rations['main_pair'],
+                           list(frequency_turns_per_min)
+                           )
 
-data_frame = pd.DataFrame()
-data_frame['frequency'] = frequency_turns_per_min
-data_frame['hub1'] = a.gear_ratio_hub1
-data_frame['hub2'] = a.gear_ratio_hub2
-data_frame['hub3'] = a.gear_ratio_hub3
-data_frame['hub4'] = a.gear_ratio_hub4
-data_frame['hub5'] = a.gear_ratio_hub5
+turnovers_wheel = pd.DataFrame()
+turnovers_wheel['frequency'] = frequency_turns_per_min
+turnovers_wheel['hub1'] = turns_wheels_service.gear_ratio_hub1
+turnovers_wheel['hub2'] = turns_wheels_service.gear_ratio_hub2
+turnovers_wheel['hub3'] = turns_wheels_service.gear_ratio_hub3
+turnovers_wheel['hub4'] = turns_wheels_service.gear_ratio_hub4
+turnovers_wheel['hub5'] = turns_wheels_service.gear_ratio_hub5
 
+#формирование данных, где вычисляется скорость автомобиля относительно кол-ва оборотов двигателя, номера передачи,
+#данных о передаточных числах каждой скорости и параметрах колёс(таблица из ecxel №5)
+wheel_info = config['data']['wheel_info']
+speed_car_service = SpeedCarService(wheel_info['profile_width'], wheel_info['profile_height'], wheel_info['diameter'],
+                    turnovers_wheel)
 
-
+speed_car = pd.DataFrame()
+speed_car['frequency'] = frequency_turns_per_min
+speed_car['hub1'] = speed_car_service.speed_hub1
+speed_car['hub2'] = speed_car_service.speed_hub2
+speed_car['hub3'] = speed_car_service.speed_hub3
+speed_car['hub4'] = speed_car_service.speed_hub4
+speed_car['hub5'] = speed_car_service.speed_hub5
--- speed_car_service.py
+++ speed_car_service.py
@@ -1,26 +1,69 @@
 from dataclasses import dataclass
+from pandas import DataFrame
+import math
 
 
 @dataclass
 class SpeedCarService:
-    profile_width:float
-    profile_height:float
-    diameter:float
-    frequency_turns_per_min: list
+    profile_width: float
+    profile_height: float
+    diameter: float
+    turnovers_wheel_data_frame: DataFrame
 
-    def _post_init_(self):
-        nominal_radius = 0.0254*(self.diameter/2)+(self.profile_width/1000)*(self.profile_height/100)
-        static_radius = nominal_radius*self.tire_crumpling_ratio()
-        dynamic_radius = nominal_radius-((nominal_radius-static_radius)/3)
+    def __post_init__(self):
+        self.nominal_radius = 0.0254 * (self.diameter / 2) + (self.profile_width / 1000) * (self.profile_height / 100)
+        self.static_radius = self.nominal_radius * self.__tire_crumpling_ratio()
+        self.dynamic_radius = self.nominal_radius - ((self.nominal_radius - self.static_radius) / 3)
+        self.turnovers_hub1 = self.turnovers_wheel_data_frame.iloc[:,
+                               1].to_numpy()  # массив кол-ва оборотов колеса на 1 скорости
+        self.turnovers_hub2 = self.turnovers_wheel_data_frame.iloc[:,
+                               2].to_numpy()  # массив кол-ва оборотов колеса на 2 скорости
+        self.turnovers_hub3 = self.turnovers_wheel_data_frame.iloc[:,
+                               3].to_numpy()  # массив кол-ва оборотов колеса на 3 скорости
+        self.turnovers_hub4 = self.turnovers_wheel_data_frame.iloc[:,
+                               4].to_numpy()  # массив кол-ва оборотов колеса на 4 скорости
+        self.turnovers_hub5 = self.turnovers_wheel_data_frame.iloc[:,
+                               5].to_numpy()  # массив кол-ва оборотов колеса на 5 скорости
+
+    @property
+    def speed_hub1(self):
+        return self.__calculate_speed_array(self.turnovers_hub1)
+
+    @property
+    def speed_hub2(self):
+        return self.__calculate_speed_array(self.turnovers_hub2)
+
+    @property
+    def speed_hub3(self):
+        return self.__calculate_speed_array(self.turnovers_hub3)
+
+    @property
+    def speed_hub4(self):
+        return self.__calculate_speed_array(self.turnovers_hub4)
+
+    @property
+    def speed_hub5(self):
+        return self.__calculate_speed_array(self.turnovers_hub5)
 
     def __tire_crumpling_ratio(self):
-        '''
+        """
             Возвращает коефициент смятия шин относительно высоты профиля колеса
-        '''
-        if(self.profile_height>=90):
+        """
+        if self.profile_height >= 90:
             return 0.8
-        elif(self.profile_height<=50):
+        elif self.profile_height <= 50:
             return 0.85
         else:
             return 0.814285714
-    def __calculate_speed_array(self, ):
+
+    def __calculate_speed_array(self, turnovers_hub):
+        """
+        Возвращает массив скорости автомобиля, относительно парамтеров колёс, кол-ва оборотов двигателя и кол-ва оборотов колеса для определённого номера скорости
+        :param turnoverse_hub: массив оборотов колеса в минуту конкретной скорости
+        :return:
+        """
+        speed_array = []
+        for turns_while in turnovers_hub:
+            speed=(self.dynamic_radius*2*3.1415926534)*turns_while/1000*60
+            speed_array.append(speed)
+        return speed_array
