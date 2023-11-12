0bc6136e66cfe4679c17b8a0b298e40ef075d6ea
Egor <egor_kuplinov@mail.ru>
2023-11-12 18:02:16 +0400

    изменгение кодировки

--- backend/config.py
+++ backend/config.py
@@ -8,5 +8,9 @@ conf = {
         'tools.sessions.on': True,
         'tools.staticdir.root': os.path.abspath(os.getcwd()),
         'tools.response_headers.headers': [('Content-Type', 'text/plain')],
+        'tools.encode.on': True,
+        'tools.encode.encoding': 'utf-8',
+        'tools.decode.on': True,
+        'tools.decode.decoding': 'utf-8',
     },
 }
