e41ab3017bb900e322b33bb52d4af05d9387d984
Denis Bondarenko <dbondarenko.work@yandex.ru>
2023-11-12 18:30:06 +0400

    Добавлена логика работы с JSON

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,16 +1,36 @@
-import { useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import "./App.css";
-import CodeEditor from '@uiw/react-textarea-code-editor';
+import CodeEditor from "@uiw/react-textarea-code-editor";
+import { useDropzone } from "react-dropzone";
 
 function App() {
   const [code, setCode] = useState<string>();
+  const decoder = useMemo(() => new TextDecoder("utf-8"),[]);
+
+  const onDrop = useCallback((acceptedFiles: File[]) => {
+    acceptedFiles[0].arrayBuffer().then((item) => decoder.decode(new Uint8Array(item))).then((item) => setCode(item));
+  }, [decoder]);
+
+  useEffect(() => {
+    console.log(code);
+  },[code])
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
 
   return (
     <>
+      <div {...getRootProps()}>
+        <input {...getInputProps()} />
+        {isDragActive ? (
+          <p>Drop the files here ...</p>
+        ) : (
+          <p>Drag 'n' drop some files here, or click to select files</p>
+        )}
+      </div>
       <CodeEditor
         value={code}
         language="json"
-        placeholder="Please enter JS code."
+        placeholder="Вставьте код JSON."
         onChange={(evn) => setCode(evn.target.value)}
         padding={15}
         style={{
