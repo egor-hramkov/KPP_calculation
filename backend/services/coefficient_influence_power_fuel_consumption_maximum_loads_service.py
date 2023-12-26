6f2995c28635b0b9024a6dffe469cc18d3f81387
Egor Khramkov <egor_kuplinov@mail.ru>
2023-12-26 17:44:37 +0400

    Удаление лишних сервисов

--- backend/services/coefficient_influence_power_fuel_consumption_maximum_loads_service.py
+++
@@ -1,6 +0,0 @@
-from dataclasses import dataclass
-
-
-@dataclass
-class CoefficientInfluencePowerFuelConsumptionMaximumLoadsService:
-    """Сервис для ормирования таблицы коэффициентов влияния мощности на расход топлива при максимальных нагрузках"""
