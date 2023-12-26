78d3a1a9a9be6d0274db47adbb2b60b6c32e37cb
Denis Bondarenko <dbondarenko.work@yandex.ru>
2023-12-26 11:45:32 +0400

    web changes

--- web/src/api/apiClient.ts
+++ web/src/api/apiClient.ts
@@ -2,6 +2,9 @@ import axios from "axios";
 
 export class kppApi {
     get() {
-        return axios.get('http://localhost:8080')
+        return axios.get('http://localhost:80')
+    }
+    post(data: any) {
+        return axios.post('http://localhost:80', data)
     }
 }
